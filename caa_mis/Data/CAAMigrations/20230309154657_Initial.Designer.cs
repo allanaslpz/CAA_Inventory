e670636081e3fbe1ed4900e87be871cb1df975fd
alopez12-nc <122580528+alopez12-nc@users.noreply.github.com>
2023-03-14 18:07:24 -0400

    Added filter on the table Connect filter from the table to the pie chart Change Transaction to Inventory Change Inventory to Inventory Report

--- caa_mis/Data/CAAMigrations/20230309154657_Initial.Designer.cs
+++ caa_mis/Data/CAAMigrations/20230309154657_Initial.Designer.cs
@@ -11,7 +11,7 @@ using caa_mis.Data;
 namespace caa_mis.Data.CAAMigrations
 {
     [DbContext(typeof(InventoryContext))]
-    [Migration("20230308165913_Initial")]
+    [Migration("20230309154657_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
