e3ca57faf765057d3ad6dde6aa91094cdb4291b2
alopez12-nc <122580528+alopez12-nc@users.noreply.github.com>
2023-03-27 21:01:47 -0400

    Fixed the LowInStock Page Added Downloadable button

--- caa_mis/Controllers/HomeController.cs
+++ caa_mis/Controllers/HomeController.cs
@@ -5,10 +5,13 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using OfficeOpenXml.FormulaParsing.Excel.Functions.Math;
+using OfficeOpenXml.Style;
 using SQLitePCL;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace caa_mis.Controllers
 {
@@ -125,5 +128,97 @@ namespace caa_mis.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
+        public ActionResult LowInStock(int branch = 0)
+        {
+            var Item = _inventoryContext.Items.Include(s => s.Stocks).ToList();
+            var Stocklist = _inventoryContext.Stocks.Where(s => s.Quantity > 0);
+            if (branch != 0)
+            {
+                Stocklist = Stocklist.Where(s => s.BranchID == branch);
+            }
+            var Category = _inventoryContext.Categories.ToList();
+            var Branch = _inventoryContext.Branches.ToList();
+
+            ViewBag.Branches = Branch.Select(b => new SelectListItem
+            {
+                Value = b.ID.ToString(),
+                Text = b.Location
+            }).ToList();
+            ViewBag.Branches.Insert(0, new SelectListItem { Value = "0", Text = "All" });
+
+            var tableItems = from item in Item
+                             join stock in Stocklist on item.ID equals stock.ItemID
+                             group stock by item.Name into t
+                             let minLevel = t.Min(s => s.MinLevel)
+                             let totalStock = t.Sum(s => s.Quantity)
+                             where totalStock < minLevel // Filter out items not low in stock
+                             select new
+                             {
+                                 ItemName = t.Key,
+                                 MinLevel = minLevel,
+                                 TotalStock = totalStock,
+                                 Percentage = 1 - ((double)minLevel / (double)totalStock)
+                             };
+            ViewBag.CurrentBranchID = branch;
+            ViewBag.TableItem = tableItems.OrderBy(s => s.Percentage);
+
+            return View(tableItems);
+        }
+
+        public IActionResult DownloadLowInStock(int branch = 0)
+        {
+            var items = _inventoryContext.Items.Include(s => s.Stocks).ToList();
+            var stocklist = _inventoryContext.Stocks.Where(s => s.Quantity > 0);
+            if (branch != 0)
+            {
+                stocklist = stocklist.Where(s => s.BranchID == branch);
+            }
+            var categories = _inventoryContext.Categories.ToList();
+            var branches = _inventoryContext.Branches.ToList();
+
+            var tableItems = from item in items
+                             join stock in stocklist on item.ID equals stock.ItemID
+                             group stock by item.Name into t
+                             let minLevel = t.Min(s => s.MinLevel)
+                             let totalStock = t.Sum(s => s.Quantity)
+                             where totalStock < minLevel // Filter out items not low in stock
+                             select new
+                             {
+                                 ItemName = t.Key,
+                                 MinLevel = minLevel,
+                                 TotalStock = totalStock
+                                 //Percentage = 1 - ((double)minLevel / (double)totalStock)
+                             };
+
+            using var excel = new ExcelPackage();
+            var worksheet = excel.Workbook.Worksheets.Add("Low In Stock Items");
+            worksheet.Cells["A1"].Value = "Low In Stock Items Report";
+
+            worksheet.Cells["A3"].LoadFromCollection(tableItems, true);
+
+            worksheet.Cells[3, 1, 3, 3].Style.Font.Bold = true;
+            worksheet.Cells[3, 1, 3, 3].Style.Fill.PatternType = ExcelFillStyle.Solid;
+            worksheet.Cells[3, 1, 3, 3].Style.Fill.BackgroundColor.SetColor(Color.LightCyan);
+
+            worksheet.Cells.AutoFitColumns();
+
+            //Since the time zone where the server is running can be different, adjust to 
+            //Local for us.
+            DateTime utcDate = DateTime.UtcNow;
+            TimeZoneInfo esTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            DateTime localDate = TimeZoneInfo.ConvertTimeFromUtc(utcDate, esTimeZone);
+
+            worksheet.Cells[1, 1].Value = "Low In Stock Items Report";
+            worksheet.Cells[1, 1].Style.Font.Bold = true;
+            worksheet.Cells[1, 1].Style.Font.Size = 14;
+            worksheet.Cells[2, 1].Value = "Created: " + localDate.ToShortTimeString() + " on " +
+                localDate.ToShortDateString();
+
+            var data = excel.GetAsByteArray();
+            var contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            var fileName = "LowInStockItems.xlsx";
+
+            return File(data, contentType, fileName);
+        }
     }
 }
