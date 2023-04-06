20ea12236f4c86f40d1863f2374572f99605b519
Luisito Jr Villaflor <lvillaflor1@ncstudents.niagaracollege.ca>
2023-04-06 18:48:41 -0400

    Prototype 4 - Change to Add Stock Item and unhide Multiple Branches and Quantities in Products.

--- caa_mis/Data/Migrations/20230406031043_Initial.Designer.cs
+++ caa_mis/Data/Migrations/20230406031043_Initial.Designer.cs
@@ -11,13 +11,13 @@ using caa_mis.Data;
 namespace caa_mis.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230320163104_Initial")]
+    [Migration("20230406031043_Initial")]
     partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "6.0.14");
+            modelBuilder.HasAnnotation("ProductVersion", "6.0.15");
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
